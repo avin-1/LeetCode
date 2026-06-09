a75e03aaaa60d4dce4aacfc8c02793d87d47eea4
Avinash Bhurke <149309916+avin-1@users.noreply.github.com>
2026-06-09 22:19:24 +0530

    LC-maximum-product-subarray [Runtime: N/A; Memory: N/A]

--- dynamic-programming/maximum-product-subarray.cpp
+++ dynamic-programming/maximum-product-subarray.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+
         // so this problem is based on maximum product's subarray based on kadane's algo on product subarray 
         // we have to maintain the maximum and minimum sum till current index since the neg no. can flip the whole game and become the largest one 
         // lets continue
-        int maxi = 1;
-        int mini = 1;
-        int ans = 0;
-        for(int i = 0;i<nums.size();i++){
+        int maxi = nums[0];
+        int mini = nums[0];
+        int ans = nums[0];
+        for(int i = 1;i<nums.size();i++){
             if(nums[i]<0){
                 swap(maxi,mini);
             }
